f938e1d9c54bd2810662bb1d5214cabd51db67fd
Юрий Малышко <yur.malishko@gmail.com>
2025-11-25 20:21:32 +0200

    install laravel breeze + bootstrap, add user role

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import { resolve } from 'path';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.scss', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            '~bootstrap-icons': resolve(__dirname, 'node_modules/bootstrap-icons'),
+        }
+    }
 });
