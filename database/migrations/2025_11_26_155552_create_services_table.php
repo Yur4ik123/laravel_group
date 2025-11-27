97da86d27d4c81c9f04472b5debb2526080ec655
Artem-dp <peakjoker33@gmail.com>
2025-11-27 11:21:22 +0200

    update services migrate

--- database/migrations/2025_11_26_155552_create_services_table.php
+++ database/migrations/2025_11_26_155552_create_services_table.php
@@ -9,6 +9,12 @@ return new class extends Migration {
     {
         Schema::create('services', function (Blueprint $table) {
             $table->id();
+            $table->string('images', 255);
+            // TODO $table->foreign('category_id')->references('id')->on('categories');
+            // "todo" after adding categories table
+            $table->foreignId('category_id')->nullable();
+            $table->string('slug');
+            $table->decimal('price', 8, 2)->default(0);
             $table->timestamps();
         });
     }
