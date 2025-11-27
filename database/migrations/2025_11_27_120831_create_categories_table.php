91fdfeaa1a77355d60c9c4d3bd4ed6ffae42df2b
Ruslan <ruslandvor1998@gmail.com>
2025-11-27 14:30:15 +0200

    migration categories and category_translate

---
+++ database/migrations/2025_11_27_120831_create_categories_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug')->unique();
+            $table->timestamps();
+        });
+
+        Schema::create('category_translations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->string('locale', 5)->index();
+            $table->string('name', 255);
+
+            $table->unique(['category_id', 'locale']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+        Schema::dropIfExists('category_translation');
+    }
+};
