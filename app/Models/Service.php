301fc22c815afe2d9ec3bb374a3d9f7a3d0bdbd2
Artem-dp <artemklicenko0@gmail.com>
2025-11-28 15:08:14 +0200

    add Service factories/seeders

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -5,9 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
 use Astrotomic\Translatable\Translatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 class Service extends Model implements TranslatableContract
 {
     use Translatable;
+    use HasFactory;
 
     public $translatedAttributes = ['name'];
 }
