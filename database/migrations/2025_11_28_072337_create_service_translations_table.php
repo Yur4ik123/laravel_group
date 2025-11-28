b542966b14214cc2b6438d11656ceb212ae8e175
Artem-dp <peakjoker33@gmail.com>
2025-11-28 09:40:24 +0200

    add service_translate migrate and model

---
+++ database/migrations/2025_11_28_072337_create_service_translations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('service_translations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('service_id')->constrained();
+            $table->string('locale')->index();
+            $table->unique(['service_id', 'locale']); // ensures that there is only one translation for each language
+                                                      // for one service
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('service_translations');
+    }
+};
