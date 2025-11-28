65bbeddd431493b934c4144017c2234574475cee
Artem-dp <artemklicenko0@gmail.com>
2025-11-28 12:10:48 +0200

    update models for services

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -3,7 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
-class Service extends Model
+use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
+use Astrotomic\Translatable\Translatable;
+class Service extends Model implements TranslatableContract
 {
+    use Translatable;
+
+    public $translatedAttributes = ['name'];
 }
--- app/Models/ServiceTranslation.php
+++ app/Models/ServiceTranslation.php
@@ -4,7 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Service_translation extends Model
+
+class ServiceTranslation extends Model
 {
-    //
+    public $timestamps = false;
 }
