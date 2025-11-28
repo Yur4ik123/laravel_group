c8820712cddac4e93f5a60fd9fa0217bc2eb692b
gessshtalt <nackilar897@gmail.com>
2025-11-28 16:33:49 +0100

    edit status

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -3,10 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory; // <- добавляем
 use Astrotomic\Translatable\Translatable;
 
 class Status extends Model
 {
+    use HasFactory; // <- добавляем
     use Translatable;
 
     public $translatedAttributes = ['name']; // переводимое поле
