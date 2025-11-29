7527aa1537cef89f3e6acfef2ae2176fa385bd0b
Ruslan <ruslandvor1998@gmail.com>
2025-11-29 17:12:16 +0200

    add Category Model, CategoryTranslation and DatabaseSeeder

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
+use Astrotomic\Translatable\Translatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
+class Category extends Model implements TranslatableContract
 {
-    //
+    use HasFactory, Translatable;
+
+    public $translatedAttributes = ['name'];
+
+    protected $fillable = [
+        'slug',
+    ];
 }
