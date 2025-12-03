11217859cd3d5e1b5d088290069e29ff9c5f3584
gessshtalt <nackilar897@gmail.com>
2025-12-03 14:08:11 +0100

    add cabinet routes

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,17 @@ Route::group(['prefix' => LaravelLocalization::setLocale()], function () {
         Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
         Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     });
+
+    Route::middleware(['auth'])->group(function () {
+
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+
+        Route::get('/profile/password', [ProfileController::class, 'password'])->name('profile.password');
+        Route::patch('/profile/password', [ProfileController::class, 'updatePassword'])->name('profile.password.update');
+
+        Route::get('/bookings', [ProfileController::class, 'bookings'])->name('profile.bookings');
+
+    });
 });
 require __DIR__ . '/auth.php';
