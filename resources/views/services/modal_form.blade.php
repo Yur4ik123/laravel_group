9018aa71cd840c8d7b108cab05b29b970b1913ae
Artem-dp <peakjoker33@gmail.com>
2025-12-05 12:49:59 +0200

    add ajax for booking form

--- resources/views/services/modal_form.blade.php
+++ resources/views/services/modal_form.blade.php
@@ -6,14 +6,18 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <form method="post" action="">
+                <form method="post" action="" id="booking-form">
                         <input type="hidden" class="form-control" id="service-reserv" name="service_id" value="{{$service->id}}">
                         <input type="hidden" class="form-control" id="date-reserv" name="date">
                         <input type="hidden" class="form-control" id="time-reserv" name="slot_id">
 
                     <div class="mb-3">
                         <label for="recipient-name" class="col-form-label">Name</label>
-                        <input type="text" class="form-control" id="client-name">
+                        <input type="text" class="form-control" id="client-name" name="client_name">
+                    </div>
+                    <div class="mb-3">
+                        <label for="recipient-name" class="col-form-label">Last name</label>
+                        <input type="text" class="form-control" id="last-name" name="last_name">
                     </div>
                     <div class="mb-3">
                         <label for="recipient-name" class="col-form-label">Email</label>
@@ -26,12 +30,13 @@
                     <div class="mb-3">
                         <label for="message-text" class="col-form-label">Comment (optional)</label>
                         <textarea class="form-control" id="message-text" name="comment"></textarea>
+                        <div class="modal-footer">
+                            <button type="submit" class="btn btn-primary">Send message</button>
+                        </div>
                     </div>
                 </form>
             </div>
-            <div class="modal-footer">
-                <button type="submit" class="btn btn-primary">Send message</button>
-            </div>
+
         </div>
     </div>
 </div>
