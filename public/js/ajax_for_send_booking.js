b6300a57708982a374d996c4f6d72402d11b9508
Artem-dp <artemklicenko0@gmail.com>
2025-12-08 17:23:18 +0200

    add some docs

--- public/js/ajax_for_send_booking.js
+++ public/js/ajax_for_send_booking.js
@@ -3,22 +3,10 @@ document.addEventListener('DOMContentLoaded', function (){
     const bookingForm = document.getElementById('booking-form');
     const sendBtn = bookingForm.querySelector('.btn');
 
-    // getting data from form
     sendBtn.addEventListener('click', async function(e){
         e.preventDefault();
         document.querySelectorAll('.input-error').forEach(item=>item.remove())
-       // const bookingData = {
-       //      // service_id:  document.getElementById('service-reserv').value,
-       //      // date:        document.getElementById('date-reserv').value,
-       //      // time:        document.getElementById('time-reserv').value,
-       //      // client_name: document.getElementById('client-name').value,
-       //      // last_name:   document.getElementById('last-name').value,
-       //      // email:       document.getElementById('client-email').value,
-       //      // phone:       document.getElementById('client-phone').value,
-       //      // comment:     document.getElementById('message-text').value,
-       // }
 
-       // sending the request
         try {
             const response = await fetch('/api/bookings', {
                 method: 'POST',
@@ -27,33 +15,21 @@ document.addEventListener('DOMContentLoaded', function (){
 
             const data = await response.json();
 
-
-            // Laravel return 422 if validation is failed
             if (!response.success) {
-                // data.errors - object with laravel errors
                 for (let field in data.errors) {
-                    // errorMessages += data.errors[field].join('\n') + '\n';
-
                     let errorBlock = document.createElement('div');
                     errorBlock.classList.add('input-error');
                     errorBlock.textContent = data.errors[field]
                     document.querySelector(`input[name=${field}]`)?.after(errorBlock);
                 }
-
                 return;
             }
-
-
-
 
             console.log('Успешный ответ от сервера:', data);
 
             window.location.href = '/thanks';
         } catch (error){
            console.log('error');
-
         }
-
-
     });
 });
